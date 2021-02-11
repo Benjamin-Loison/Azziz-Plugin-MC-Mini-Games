3b761713077b946c2a3332ea499233b1b48465de
Benjamin Loison <benjamin.loison@orange.fr>
2021-02-11 18:09:27 +0100

    Update FileAPI.java

--- src/fr/azzizcouscous/benjaminloison/api/FileAPI.java
+++ src/fr/azzizcouscous/benjaminloison/api/FileAPI.java
@@ -1,4 +1,4 @@
-package fr.azzizcouscous.benjaminloison.api;
+package fr.mcminigames.benjaminloison.api;
 
 import java.io.File;
 import java.io.FileWriter;
@@ -6,16 +6,16 @@ import java.io.IOException;
 
 import org.apache.commons.lang3.StringUtils;
 
-import fr.azzizcouscous.benjaminloison.main.Main;
+import fr.mcminigames.benjaminloison.main.Main;
 
 public class FileAPI
 {
-    public final static String path = new File("").getAbsolutePath() + File.separatorChar, pluginFolder = path + "plugins" + File.separatorChar, azzizFolder = pluginFolder + Main.NAME + File.separatorChar, gamesFolder = azzizFolder + "games" + File.separatorChar, schematicsFolder = FileAPI.pluginFolder + "WorldEdit" + File.separatorChar + "schematics" + File.separatorChar;
-    public final static File languageFile = new File(FileAPI.azzizFolder + "language.txt");
+    public final static String path = new File("").getAbsolutePath() + File.separatorChar, pluginFolder = path + "plugins" + File.separatorChar, mcminigamesFolder = pluginFolder + Main.NAME + File.separatorChar, gamesFolder = mcminigamesFolder + "games" + File.separatorChar, schematicsFolder = FileAPI.pluginFolder + "WorldEdit" + File.separatorChar + "schematics" + File.separatorChar;
+    public final static File languageFile = new File(FileAPI.mcminigamesFolder + "language.txt");
 
     public static void initialize()
     {
-        new File(FileAPI.azzizFolder).mkdirs();
+        new File(FileAPI.mcminigamesFolder).mkdirs();
         new File(FileAPI.gamesFolder).mkdirs();
         new File(FileAPI.schematicsFolder).mkdirs();
     }
